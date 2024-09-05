93095439743baf0bedb5668facde7eb8023c191f
Cole Wulterkens <cole@wulterkens.me>
2024-09-05 00:09:39 -0500

    Revert "Initial commit"
    This reverts commit 68c64876d28e67b1d7ee38a113ea18905159b73a.

--- webroot/phoenix/js/search.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.search-form').submit(function(e) {
-        e.preventDefault();
-        var searchValue = $('input[name="query"]').val();
-        window.location.href = 'https://www.myhomelearner.com/search/' + searchValue;
-    });
-});
